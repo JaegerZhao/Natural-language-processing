67cba7949db3bac55723951041490710e951dce1
JaegerZhao <zhaoyiyang_jaeger@163.com>
2024-04-28 07:45:00 +0800

    NLP

--- 1 Word2VecTransE/TransE/TransE.py
+++ 1 Word2VecTransE/TransE/TransE.py
@@ -62,13 +62,13 @@ class TransE(nn.Module):
 
     def _calc(self, h, t, r):
         # TO DO: implement score function
-        # Hint: you can use torch.normalize and torch.norm functions
+        # Hint: you can use F.normalize and torch.norm functions
         if self.norm_flag: # normalize embeddings with l2 norm
-            h = F.normalize(h,p=2,dim = -1)
-            t = F.normalize(t,p=2,dim = -1)
-            r = F.normalize(r,p=2,dim = -1)
+            h = F.normalize(h,p=2,dim = 2)
+            t = F.normalize(t,p=2,dim = 2)
+            r = F.normalize(r,p=2,dim = 2)
         score = h+r-t
-        score = torch.norm(score,self.p_norm,dim = -1)
+        score = torch.norm(score,self.p_norm,dim = 2)
         return score
 
 
@@ -90,10 +90,7 @@ class TransE(nn.Module):
     def loss(self, pos_score, neg_score):
         # TO DO: implement loss function
         # Hint: consider margin
-        loss_func = nn.MarginRankingLoss(self.margin.item(),size_average = False)
-        y = torch.Tensor([1])
-        Loss = loss_func(neg_score,pos_score,y)
-        return Loss
+        return torch.nn.ReLU()(self.margin + (pos_score - neg_score).mean())
         
 
 
